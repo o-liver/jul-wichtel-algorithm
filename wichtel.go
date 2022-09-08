fe01f72197bee1ff2b92716fbd316b8fcbc99312
Oliver Feldmann <oliver.feldmann@sap.com>
2022-09-08 12:08:17 +0200

    Match boomers with millenials

--- wichtel.go
+++ wichtel.go
@@ -23,17 +23,44 @@ func main() {
 	fmt.Println("Adding millennial's twice into the hat")
 	theHat := append(millennials, millennials...)
 
-	rand.Seed(time.Now().UnixNano())
-	rand.Shuffle(len(theHat), func(i, j int) {
-		theHat[i], theHat[j] = theHat[j], theHat[i]
-	})
-	fmt.Println("After mixing the hat we got:", theHat)
+	// Shuffle the participants in the hat
+	shuffleTheHat(theHat)
 
+	wichtelMatches := make(map[string][]string, len(participants))
+	for _, boomer := range babyBoomers {
+		var firstMatch, secondMatch string
+		firstSlipOfPaperIndex := 0
+		firstMatch = theHat[firstSlipOfPaperIndex]
+		theHat = removeSlipOfPaperWithIndex(theHat, firstSlipOfPaperIndex)
+		for index, slipOfPaper := range theHat[1:] { // Iterate over slips of paper in the hat starting at the second entry
+			secondMatch = slipOfPaper
+			if secondMatch != firstMatch { // If the first and second match are not the same we have the result we want
+				theHat = removeSlipOfPaperWithIndex(theHat, index) // overwrite hat removing the first entry
+				break // we can break our of the for loop
+			} // else we go to the next slip of paper
+		}
+		wichtelMatches[boomer] = []string{firstMatch, secondMatch}
+	}
 
+	fmt.Println(wichtelMatches) // TODO: Remove this debug message!
 
 }
 
-func sendOutEmailsAndDeleteThemAfter(wichtelMatches map[string]string) {
+func removeSlipOfPaperWithIndex(hat []string, index int) []string {
+	newHat := make([]string, 0)
+	newHat = append(newHat, hat[:index]...)   // Retrieve entries before index
+	newHat = append(newHat, hat[index+1:]...) // Retrieve entries after index
+	return newHat
+}
+
+func shuffleTheHat(hat []string) {
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(hat), func(i, j int) {
+		hat[i], hat[j] = hat[j], hat[i]
+	})
+}
+
+func sendOutEmailsAndDeleteThemAfter(wichtelMatches map[string][]string) {
 	token := os.Getenv("ACCESS_TOKEN")
 	fmt.Println("This is your secret:", token)
 }
